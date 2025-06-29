223f1432a811f2203ad8d404d64043e01d13fcfd
binikiya <biniyamkiya3682@gmail.com>
2025-06-29 21:35:56 +0300

    Intoduction to DB

---
+++ MySQLServer.py
@@ -0,0 +1,28 @@
+import mysql.connector
+from mysql.connector import Error
+
+def create_database():
+    try:
+        connection = mysql.connector.connect(
+            host='localhost',
+            user='root',
+            password='bini'
+        )
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as err:
+        print(f"Error: {err}")
+
+    finally:
+        if 'cursor' in locals() and cursor:
+            cursor.close()
+        if 'connection' in locals() and connection.is_connected():
+            connection.close()
+            print("MySQL connection is closed.")
+
+if __name__ == "__main__":
+    create_database()
