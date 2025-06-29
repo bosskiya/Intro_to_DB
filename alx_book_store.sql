0a8882d26fb1893ff308ef0298a3c3314eb292da
binikiya <biniyamkiya3682@gmail.com>
2025-06-29 21:17:21 +0300

    Intoduction to DB

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -24,13 +24,13 @@ CREATE TABLE IF NOT EXISTS Customers (
 
 CREATE TABLE IF NOT EXISTS Orders (
     order_id INT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGH KEY (customer_id) REFERENCES Customers(customer_id),
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
     order_date DATE
 )
 
 CREATE TABLE IF NOT EXISTS Order_Details (
     orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
-    FOREIGH KEY (order_id) REFERENCES Orders(order_id),
-    FOREIGH KEY (book_id) REFERENCES Books(book_id),
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id),
     quantity DOUBLE
 )
